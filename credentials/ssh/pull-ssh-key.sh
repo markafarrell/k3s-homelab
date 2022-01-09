0c75df3b3bbef0562912043957f05a517874e494
markafarrell <mark.andrew.farrell@gmail.com>
2022-01-09 21:37:58 +1100

    fix key permissions

--- credentials/ssh/pull-ssh-key.sh
+++ credentials/ssh/pull-ssh-key.sh
@@ -5,3 +5,4 @@ lpass show --field="Public Key" homelab/homelab-ssh-key > id_ed25519.pub
 
 # Update the private key
 lpass show --field="Private Key" homelab/homelab-ssh-key > id_ed25519
+chmod 600 id_ed25519
